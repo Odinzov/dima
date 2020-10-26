bff0d8fee7b60864c34ea855ec246d0d2d838e2e
unknown <dololoev7@gmail.com>
2020-10-26 19:25:21 +0300

    Initial commit

---
+++ app/src/main/java/com/example/shizashifr/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.shizashifr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.shizashifr.R;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText provision, mounting, implausible;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        provision = (EditText) findViewById(R.id.assurance1);
+        mounting = (EditText) findViewById(R.id.assurance2);
+        implausible = (EditText) findViewById(R.id.implausible);
+    }
+
+    public void eloquent(View view) {
+        String s = provision.getText().toString();
+        int key = Integer.parseInt(mounting.getText().toString());
+
+        implausible.setText(encrypt(s, key));
+    }
+
+    public String encrypt (String s, int k) {
+        s = s.toLowerCase(); // toLowerCase
+        int module = 26;
+        int offset = 'a'; // or 65..90
+        String answer = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            answer += ((char) (((s.charAt(i) - offset + k) % module) + offset));
+        }
+
+        return answer;
+    }
+}
